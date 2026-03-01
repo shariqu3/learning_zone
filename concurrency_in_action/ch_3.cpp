a0bfb1198e1600dab82a29e8c019e61e3759d11e
Sharique <sharique@tamu.edu>
2026-02-28 22:06:33 -0600

    Completed till 3.2.1

---
+++ concurrency_in_action/ch_3.cpp
@@ -0,0 +1,51 @@
+#include <algorithm>
+#include <iostream>
+#include <list>
+#include <mutex>
+#include <thread>
+/* Chapter covers
+  Problem sharing data b/w thread and fixes */
+
+// 1. Invariants:  Statements that are always true about a particular data
+// structure
+// 2. Race condition: If the outcome depends on the relative ordering of two
+// reads running in parallel
+// 3. When race condition breaks invariants then it is a problem
+// 4. Data race: Race condition that arises to due concurrent modifications to a
+// single object
+// 5. lock-free programming: Design data structures such that invariants
+// modifications codes as a sereis of indivisible changes.
+// 6. Dealing with race conditions:
+//    1. Protection mechanism to allow one thread to modify data
+//    2. Lock free programming
+//    3. Softawre Transactional Memory: Transaction based updates, the
+//    intermediate steps are recorded and executed altogether, if any issue
+//    occurs it is rolleback.
+//    4. Using mutex (mutually exclusive)
+
+class Node {
+  int val;
+  Node *prev, *next;
+  Node() : val(0), prev(nullptr), next(nullptr) {}
+};
+
+class DLL {
+  Node *head;
+  DLL() : head(nullptr) {}
+  // void  /
+};
+
+std::list<int> l;
+std::mutex m;
+
+void add_to_list(int x) {
+  std::lock_guard<std::mutex> guard(m);
+  l.push_back(x);
+}
+
+bool contains(int x) {
+  std::lock_guard<std::mutex> guard(m);
+  return std::find(l.begin(), l.end(), x) != l.end();
+}
+
+int main() { return 0; }
